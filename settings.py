244314ff579f82e66ef53c5e1b88581972e6b958
eddieKarlsson <eddieperty@gmail.com>
2020-08-23 17:08:05 +0300

    first commit

---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """A class to store all settins for Alien Invasion."""
+    def __init__(self):
+        """Initiliaze the game's settings."""
+        # Screen Settings
+        self.screen_width = 1200
+        self.screen_height = 750
+        self.bg_color = (33, 37, 43)
