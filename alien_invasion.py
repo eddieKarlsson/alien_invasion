de5d8970d6fedfc824c5489e92677d195142d275
eddieKarlsson <eddieperty@gmail.com>
2020-08-26 20:44:22 +0300

    Update bullets feature

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,13 +32,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
-
-            # Copy bullets list and check if its on the top, if so remove it.
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-
+            self._update_bullets()
             self._update_screen()
 
     def _check_events(self):
@@ -64,8 +58,18 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group"""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        # Update bullets
+        self.bullets.update()
+
+        # Copy bullets list and check if its on the top, if so remove it.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _check_keyup_events(self, event):
         """Respond to keypreleases"""
